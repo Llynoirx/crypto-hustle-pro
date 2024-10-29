c59febee3c1894c7e27d7f435456e73a61d51d02
llynoirx <kathyho04@gmail.com>
2024-10-28 23:49:48 -0400

    checkpoint 5?

--- src/Components/CoinDetail.jsx
+++ src/Components/CoinDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import CoinChart from "./CoinChart";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const CoinDetail = () => {
@@ -98,6 +99,11 @@ const CoinDetail = () => {
             </tr>
         </tbody>
         </table>
+
+        <CoinChart
+        symbol={params.symbol}
+        market={fullDetails.numbers[params.symbol].USD.MARKET}
+        />
         </>
     );
   };
