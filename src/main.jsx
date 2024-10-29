367a213fd146515f945e25805a67a083843bfd42
llynoirx <kathyho04@gmail.com>
2024-10-28 23:40:42 -0400

    chexkpoint 2 done? (pics not loading so cant check

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from './App.jsx'
 import Layout from '../routes/Layout';
+import DetailView from '../routes/DetailView';
+import './index.css'
+
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
 <Routes>
     <Route path="/" element={<Layout/>}>
       <Route index={true} path="/" element={<App />} />
+      <Route index={false} path="/coinDetails/:symbol" element={<DetailView />} />
     </Route>
 </Routes>
 </BrowserRouter>
