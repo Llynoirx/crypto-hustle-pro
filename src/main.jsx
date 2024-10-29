a9d5d21462a61d13d0babce6cb69080055225a19
llynoirx <kathyho04@gmail.com>
2024-10-28 23:31:25 -0400

    checkpoint 1 done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from '../routes/Layout';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+<Routes>
+    <Route path="/" element={<Layout/>}>
+      <Route index={true} path="/" element={<App />} />
+    </Route>
+</Routes>
+</BrowserRouter>
 )
